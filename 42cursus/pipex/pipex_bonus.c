766fbd098630ac8e51efef0dce05b1524fc9e898
falconlee236 <falconlee236@cau.ac.kr>
2023-08-10 23:25:42 +0900

    new bonus file

---
+++ 42cursus/pipex/pipex_bonus.c
@@ -0,0 +1,5 @@
+//
+// Created by 이상윤 on 2023/08/10.
+//
+
+#include "pipex_bonus.h"
---
+++ 42cursus/pipex/pipex_bonus.h
@@ -0,0 +1,23 @@
+//
+// Created by 이상윤 on 2023/08/10.
+//
+
+#ifndef INC_42SEOUL_ARCHIVE_PIPEX_BONUS_H
+#define INC_42SEOUL_ARCHIVE_PIPEX_BONUS_H
+
+# include "./libft/libft.h"
+# include <unistd.h>
+/*access() unlink() close() read() write() pipe() dup() dup2() execve() fork()*/
+# include <fcntl.h>
+/* open() */
+# include <stdio.h>
+/* perror() */
+# include <string.h>
+/* strerror */
+# include <sys/wait.h>
+/* waitpid() wait() */
+# include <stdlib.h>
+/* malloc() free() exit() */
+# include <errno.h>
+
+#endif //INC_42SEOUL_ARCHIVE_PIPEX_BONUS_H
