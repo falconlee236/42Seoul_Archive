a898701fdaa9cddb92c2590600f32c41dc38a64b
falconlee236 <falconlee236@cau.ac.kr>
2023-08-17 10:40:15 +0900

    init header file include

--- 42cursus/philosophers/philo/philo.h
+++ 42cursus/philosophers/philo/philo.h
@@ -13,6 +13,26 @@
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <stdio.h>
+/* in mandatory */
+# include <stdio.h> /* printf */
+# include <stdlib.h> /* memset malloc free */
+# include <unistd.h> /* write, usleep */
+# include <sys/time.h> /* gettimeofday */
+# include <pthread.h>
+/* pthread_create, pthread_detach, pthread_join
+ * pthread_mutex_init, pthread_mutex_destory,
+ * pthread_mutex_lock, pthread_mutex_unlock */
 
+/* in bonus */
+/*
+# include <stdio.h> // printf
+# include <stdlib.h> // memset, malloc, free, exit
+# include <unistd.h> // fork, write, usleep
+# include <sys/wait.h> //waitpid
+# include <signal.h> // kill
+# include <sys/time.h> // gettimeofday
+# include <pthread.h> // pthread_create, pthread_detach, pthread_join
+# include <semaphore.h>
+// sem_open, sem_close, sem_post, sem_wait, sem_unlink
+*/
 #endif
