9eab350430c50e317ef4df9321757408c378e8bb
falconlee236 <falconlee236@cau.ac.kr>
2023-08-21 14:17:49 +0900

    a

--- 42cursus/philosophers/philo/philo.c
+++ 42cursus/philosophers/philo/philo.c
@@ -6,18 +6,59 @@
 /*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 13:44:11 by sangylee          #+#    #+#             */
-/*   Updated: 2023/08/21 13:46:15 by sangylee         ###   ########.fr       */
+/*   Updated: 2023/08/21 14:14:03 by sangylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+long long	ft_atoll(char *str)
+{
+	int			i;
+	int			flag;
+	long long	res;
+
+	i = 0;
+	res = 0;
+	flag = 1;
+	while (str[i] && !(str[i] >= '0' && str[i] <= '9'))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			flag = -1;
+		i++;
+	}
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+		res = (res * 10LL) + (str[i++] - '0');
+	return (res * flag);
+}
+
+int	philo_init(t_data *data, int ac, char **av)
+{
+	data->philo_num = ft_atoll(av[1]);
+	data->die_time = ft_atoll(av[2]);
+	data->eat_time = ft_atoll(av[3]);
+	data->sleep_time = ft_atoll(av[4]);
+	if (ac == 6)
+		data->must_eat = ft_atoll(av[5]);
+	data->philos = (pthread_t *)malloc(
+			sizeof(pthread_t) * (data->philo_num + 1));
+	if (!data->philos)
+		return (0);
+	data->forks = (pthread_mutex_t *)malloc(
+			sizeof(pthread_mutex_t) * (data->philo_num + 1));
+	if (!data->forks)
+	{
+		free(data->philos);
+		return (0);
+	}
+}
+
 int	main(int ac, char **av)
 {
-	struct timeval	t1;
+	t_data	data;
 
-	if (ac < 5 || ac > 6)
+	if (ac < 5 || ac > 6 || !philo_init(&data, ac, av))
 		return (0);
-	gettimeofday(&t1, 0);
-	printf("%ld %s\n", t1.tv_sec, av[0]);
 }
--- 42cursus/philosophers/philo/philo.h
+++ 42cursus/philosophers/philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 19:12:39 by sangylee          #+#    #+#             */
-/*   Updated: 2023/08/16 19:14:10 by sangylee         ###   ########.fr       */
+/*   Updated: 2023/08/21 14:05:30 by sangylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,14 @@
  * pthread_mutex_init, pthread_mutex_destory,
  * pthread_mutex_lock, pthread_mutex_unlock */
 
-/* in bonus */
-/*
-# include <stdio.h> // printf
-# include <stdlib.h> // memset, malloc, free, exit
-# include <unistd.h> // fork, write, usleep
-# include <sys/wait.h> //waitpid
-# include <signal.h> // kill
-# include <sys/time.h> // gettimeofday
-# include <pthread.h> // pthread_create, pthread_detach, pthread_join
-# include <semaphore.h>
-// sem_open, sem_close, sem_post, sem_wait, sem_unlink
-*/
+typedef struct s_data
+{
+	int				philo_num;
+	int				die_time;
+	int				eat_time;
+	int				sleep_time;
+	int				must_eat;
+	pthread_t		*philos;
+	pthread_mutex_t	*forks;
+}	t_data;
 #endif
