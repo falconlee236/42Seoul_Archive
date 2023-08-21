70dc5bff8a8655c3aae20a57c8d51e4e2e5406eb
falconlee236 <falconlee236@cau.ac.kr>
2023-08-21 13:46:32 +0900

    a

--- 42cursus/philosophers/philo/philo.c
+++ 42cursus/philosophers/philo/philo.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/16 19:12:21 by sangylee          #+#    #+#             */
-/*   Updated: 2023/08/16 19:14:48 by sangylee         ###   ########.fr       */
+/*   Created: 2023/08/21 13:44:11 by sangylee          #+#    #+#             */
+/*   Updated: 2023/08/21 13:46:15 by sangylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 int	main(int ac, char **av)
 {
-    struct timeval  t1;
+	struct timeval	t1;
+
 	if (ac < 5 || ac > 6)
-        return (0);
-    gettimeofday(&t1, 0);
-    printf("%ld %s\n", t1.tv_sec, av[0]);
+		return (0);
+	gettimeofday(&t1, 0);
+	printf("%ld %s\n", t1.tv_sec, av[0]);
 }
