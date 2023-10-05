90d3934a948acc5859101c7694d31a615b51033c
falconlee236 <falconlee236@cau.ac.kr>
2023-10-05 20:04:20 +0900

    set Makefile

---
+++ 42cursus/philosophers/philo_bonus/philo.c
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/21 13:44:11 by sangylee          #+#    #+#             */
+/*   Updated: 2023/10/05 19:49:03 by sangylee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	ft_atoi(const char *str, int *f)
+{
+	int			i;
+	int			flag;
+	long long	res;
+
+	i = 0;
+	flag = 1;
+	res = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i++] == '-')
+			flag = -1;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res *= 10;
+		res += str[i++] - '0';
+		if (res >= 2147483647 || res < -2147483648)
+		{
+			*f = 1;
+			return (0);
+		}
+	}
+	return (res * flag);
+}
+
+int	ft_mutex_init(t_data *data)
+{
+	int	cnt;
+
+	cnt = 0;
+	while (cnt < data->philo_num)
+	{
+		if (pthread_mutex_init(&data->forks[cnt++], 0) != 0)
+		{
+			free(data->forks);
+			return (0);
+		}
+	}
+	pthread_mutex_init(&data->m_mutex, 0);
+	pthread_mutex_init(&data->eat_mutex, 0);
+	pthread_mutex_init(&data->eat_cnt_mutex, 0);
+	return (1);
+}
+
+int	case_one(t_data *data)
+{
+	printf("\033[0;3%dm%d %d %s\n\033[0m", 1, 0, 1, "has taken a fork");
+	usleep_interval(data->die_time);
+	printf("\033[0;3%dm%d %d %s\n\033[0m", 1, data->die_time, 1, "is die");
+	return (0);
+}
+
+int	philo_init(t_data *data, int ac, char **av)
+{
+	int	flag;
+
+	flag = 0;
+	data->philo_num = ft_atoi(av[1], &flag);
+	data->die_time = ft_atoi(av[2], &flag);
+	data->eat_time = ft_atoi(av[3], &flag);
+	data->sleep_time = ft_atoi(av[4], &flag);
+	if (data->die_time <= 0 || data->eat_time <= 0 || data->sleep_time <= 0)
+		return (0);
+	data->must_eat = -1;
+	data->monitor = 0;
+	data->total_eat_cnt = 0;
+	if (ac == 6)
+		data->must_eat = ft_atoi(av[5], &flag);
+	if ((data->must_eat <= 0 && ac == 6) || flag)
+		return (0);
+	if (data->philo_num == 1)
+		return (case_one(data));
+	data->forks = (pthread_mutex_t *)malloc(
+			sizeof(pthread_mutex_t) * data->philo_num);
+	if (!data->forks || !ft_mutex_init(data))
+		return (0);
+	return (1);
+}
+
+int	main(int ac, char **av)
+{
+	t_data	data;
+	t_philo	*philos;
+	int		i;
+
+	if (ac < 5 || ac > 6 || !philo_init(&data, ac, av))
+		return (0);
+	philos = (t_philo *)malloc(sizeof(t_philo) * data.philo_num);
+	if (!philos)
+	{
+		free(data.forks);
+		return (0);
+	}
+	run_philo(&data, philos);
+	i = -1;
+	while (++i < data.philo_num)
+		pthread_mutex_destroy(&data.forks[i]);
+	free(data.forks);
+	free(philos);
+}
---
+++ 42cursus/philosophers/philo_bonus/philo_util.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_util.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangylee <sangylee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 14:03:37 by isang-yun         #+#    #+#             */
+/*   Updated: 2023/10/05 19:46:56 by sangylee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long long	ft_get_time(void)
+{
+	struct timeval	tv;
+	long long		tv_res;
+
+	gettimeofday(&tv, 0);
+	tv_res = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	return (tv_res);
+}
+
+int	check_eat(t_data *data)
+{
+	pthread_mutex_lock(&data->eat_cnt_mutex);
+	if (data->total_eat_cnt == data->philo_num)
+	{
+		pthread_mutex_lock(&data->m_mutex);
+		data->monitor = 1;
+		pthread_mutex_unlock(&data->m_mutex);
+		pthread_mutex_unlock(&data->eat_cnt_mutex);
+		return (1);
+	}
+	pthread_mutex_unlock(&data->eat_cnt_mutex);
+	return (0);
+}
+
+void	check_die(t_data *data, t_philo *philos)
+{
+	int	i;
+	int	flag;
+
+	flag = 0;
+	while (!flag)
+	{
+		i = -1;
+		if (check_eat(data) == 1)
+			return ;
+		while (++i < data->philo_num)
+		{
+			pthread_mutex_lock(&data->eat_mutex);
+			if (ft_get_time() - philos[i].last_time >= data->die_time)
+			{
+				ft_print_format(&philos[i], "died");
+				pthread_mutex_lock(&data->m_mutex);
+				data->monitor = 1;
+				flag = 1;
+				pthread_mutex_unlock(&data->m_mutex);
+				pthread_mutex_unlock(&data->eat_mutex);
+				break ;
+			}
+			pthread_mutex_unlock(&data->eat_mutex);
+		}
+	}
+}
+
+void	usleep_interval(long long t)
+{
+	long long	target_time;
+
+	target_time = t + ft_get_time();
+	while (target_time > ft_get_time())
+		usleep(100);
+}
+
+void	ft_print_format(t_philo *philo, char *format)
+{
+	long long	timestamp;
+
+	timestamp = ft_get_time() - philo->init_time;
+	pthread_mutex_lock(&philo->data->m_mutex);
+	if (philo->data->monitor)
+	{
+		pthread_mutex_unlock(&philo->data->m_mutex);
+		return ;
+	}
+	pthread_mutex_unlock(&philo->data->m_mutex);
+	printf("\033[0;3%dm%lld %d %s\n\033[0m",
+		philo->id % 8, timestamp, philo->id, format);
+}
