24ea1741aa0c663e958038915a5cffe35876423d
falconlee236 <falconlee236@cau.ac.kr>
2024-03-13 22:32:58 +0900

    finish ex00

--- 42cursus/cpp_module/module00/ex00/megaphone.cpp
+++ 42cursus/cpp_module/module00/ex00/megaphone.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main(int ac, char **av){
-	std::cout << av[0] << " " << ac << "\n";
+	if (ac == 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	}
+	else {
+		for(int i = 1; av[i]; i++){
+			for(int j = 0; av[i][j]; j++){
+				std::cout << static_cast<char>(std::toupper(av[i][j]));
+			}
+		}
+		std::cout << "\n";
+	}
 	return 0;
 }
