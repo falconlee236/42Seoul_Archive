dbe349577cd3ad62c6db08e8820f327e824dc6e3
falconlee236 <falconlee236@cau.ac.kr>
2024-03-14 22:49:40 +0900

    finish ex02

--- 42cursus/cpp_module/module00/ex02/Account.cpp
+++ 42cursus/cpp_module/module00/ex02/Account.cpp
@@ -69,7 +69,7 @@ void Account::makeDeposit(int deposit){
     _displayTimestamp();
     std::cout << "index:" << this->_accountIndex;
     std::cout << ";p_amount:" << this->_amount - deposit;
-    std::cout << ";deposits:" << deposit;
+    std::cout << ";deposit:" << deposit;
     std::cout << ";amount:" << this->_amount;
     std::cout << ";nb_deposits:" << this->_nbDeposits << "\n";
 }
