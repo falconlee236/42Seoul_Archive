1bab4ea27859845f1dd68a73e10f70e2fd610b6b
falconlee236 <falconlee236@cau.ac.kr>
2024-04-25 23:24:55 +0900

    add FragTRAP

--- 42cursus/cpp_module/cpp03/ex02/FragTrap.cpp
+++ 42cursus/cpp_module/cpp03/ex02/FragTrap.cpp
@@ -1,7 +1,6 @@
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap(void)
-{
+FragTrap::FragTrap(void){
     this->_name = "default";
     this->_hit = 100;
     this->_energy = 100;
@@ -9,17 +8,15 @@ FragTrap::FragTrap(void)
     std::cout << "FragTrap " << this->_name << " constructor called\n";
 }
 
-FragTrap::FragTrap(std::string name)
-{
+FragTrap::FragTrap(std::string name){
     this->_name = name;
     this->_hit = 100;
     this->_energy = 100;
-    this->_attack = 40;
+    this->_attack = 30;
     std::cout << "FragTrap " << this->_name << " constructor called\n";
 }
 
-FragTrap::FragTrap(const FragTrap &ref)
-{
+FragTrap::FragTrap(const FragTrap &ref){
     this->_name = ref._name;
     this->_hit = ref._hit;
     this->_energy = ref._energy;
@@ -27,8 +24,7 @@ FragTrap::FragTrap(const FragTrap &ref)
     std::cout << "FragTrap " << this->_name << " copy constructor called\n";
 }
 
-FragTrap &FragTrap::operator=(const FragTrap &ref)
-{
+FragTrap &FragTrap::operator=(const FragTrap &ref){
     if (this != &ref)
     {
         this->_name = ref._name;
@@ -40,11 +36,15 @@ FragTrap &FragTrap::operator=(const FragTrap &ref)
     return *this;
 }
 
-FragTrap::~FragTrap(void)
-{
+FragTrap::~FragTrap(void){
     std::cout << "FragTrap " << this->_name << " destructor called\n";
 }
 
 void FragTrap::highFiveGuys(void){
-    std::cout << "FragTrap " << this->_name << " displays a high fives request\n";
+    if (this->_energy == 0 || this->_hit == 0){
+        std::cout << "FragTrap " << this->_name << " cannot do anything\n";
+    }
+    else{
+        std::cout << "FragTrap " << this->_name << " displays a high fives request\n";
+    }
 }
