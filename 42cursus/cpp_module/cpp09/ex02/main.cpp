ff5311c5cf24d59fb9f4e318b9d76648102992df
falconlee236 <falconlee236@cau.ac.kr>
2024-07-05 16:42:26 +0900

    add error handling

--- 42cursus/cpp_module/cpp09/ex02/main.cpp
+++ 42cursus/cpp_module/cpp09/ex02/main.cpp
@@ -7,10 +7,19 @@ int main(int ac, char **av){
     }
     std::vector<int> arr;
     for(int i = 1; av[i]; i++){
-        int num = std::atoi(av[i]);
-        if (num <= 0) {
+        long long num = std::atoll(av[i]);
+        if (num <= 0 || num < -2147483648 || num > 2147483647) {
             std::cout << "Invalid value\n";
             return 1;
+        }
+        int j = 0;
+        if (av[i][0] == '+' || av[i][0] == '-') j++;
+        while (av[i][j]){
+            if (av[i][j] > '9' || av[i][j] < '0'){
+                std::cout << "Invalid value\n";
+                return 1;
+            }
+            j++;
         }
         arr.push_back(num);
     }
